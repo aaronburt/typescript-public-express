40eb5671318b5aa3045584d1e863984fd1250ce6
aaronburt <email@aaronburt.co.uk>
2022-10-21 18:54:18 +0100

    Change 404 status to json

--- index.ts
+++ index.ts
@@ -7,6 +7,6 @@ const { EXPRESS_PORT } = process.env;
 
 app.use(express.static('/public'));
 
-app.all('*', (req: Request, res: Response) => { return res.sendStatus(404); });
+app.all('*', (req: Request, res: Response) => { return res.status(404).json({ "status": 404 }); });
 
 app.listen(EXPRESS_PORT, () => { console.log(`Started on port: ${EXPRESS_PORT}`); });
